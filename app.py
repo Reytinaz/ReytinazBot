24aa1b779adc20b2b77845fdf6c43a9ceedd34a0
Reytinaz <akakijvesnuskin@gmail.com>
2023-11-03 14:55:40 +0300

    test 2

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from threading import Thread
 
-app = Flask(__name__)
+app = Flask('')
 
-@app.route("/")
+@app.route('/')
+def home():
+  return "Hello!"
+
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
 def keep_alive():
-    return "Hello, World!"
+  t = Thread(target=run)
+  t.start()
